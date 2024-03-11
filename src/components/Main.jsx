6c682eeecdbfda7e6c7e3574a44995ac243269ff
abdala-aljewarane <108558039+abdala-aljewarane@users.noreply.github.com>
2024-03-11 15:02:07 -0500

    added left sidenav + background

---
+++ src/components/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main'>
+        <img className='w-full h-screen object-cover object-left scale-x-[-1]' src="https://import.cdn.thinkific.com/258673%2Fcustom_site_themes%2Fid%2FS7iLhz2vSuSjmBc4MSDW_New%20TE%20Logo.jpg"></img>
+       
+        </div>
+  )
+}
+
+export default Main
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -35,7 +35,7 @@ const Sidenav = () => {
             className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-200'
           >
             <AiOutlineBook size={20} />
-            <span className='pl-4'>Write Books</span>
+            <span className='pl-4'>Write a Book</span>
           </a>
           <a
             href='#main'
@@ -46,8 +46,26 @@ const Sidenav = () => {
           </a>
         </div>
       ) : (
-        <div></div>
+        ''
       )}
+        <div className='md:block hidden fixed top-[25%] z-10'>
+          <div className='flex flex-col'>
+            <a href="#main" className='rounded-full shadow rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineHome size={20}/>
+            </a>
+            <a href="#main" className='rounded-full shadow rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineAppstore size={20}/>
+            </a>
+            <a href="#main" className='rounded-full shadow rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineBook size={20}/>
+            </a>
+            <a href="#main" className='rounded-full shadow rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 ease-in duration-300'>
+              <AiOutlineHighlight size={20}/>
+            </a>
+
+          </div>
+        </div>
+
     </div>
   );
 };
